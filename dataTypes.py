10d0c6ee40b36c44de25a3edfe1cda892f9ae2e3
crisvalveslds <cristiano.valves@me.com>
2025-11-05 19:36:08 -0600

    Changes todya

---
+++ dataTypes.py
@@ -0,0 +1,10 @@
+age = input("How old are you? ")
+print(f"On your next birthday, you will be {int(age) + 1} years old.")
+
+eggs = input("How many egg cartons do you have? ")
+print(f"You have {int(eggs) * 12} eggs.")
+
+cookies = input("How many cookies do you have? ")
+people = input("How many people are there to share the cookies? ")
+print(f"Each person may have {int(cookies) // int(people)} cookies and there will be {int(cookies) % int(people)} cookies left over.")
+
